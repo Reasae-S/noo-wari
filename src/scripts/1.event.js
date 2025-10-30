1770a7b3af09e6c98c7cc56cc6b811da1fcf57d5
Reasae <reasae@proton.me>
2025-10-30 08:54:07 -0300

    Chapter 4: The Individuality of the Self

--- src/scripts/1.event.js
+++ src/scripts/1.event.js
@@ -1,65 +1,63 @@
 void function () {
-  var events = new Object();
-  
-  var defaultParent;
-  var possibleParents = new Array(document, window, globalThis);
+  var defaultParent = null;
+  var possibleParents = new Array();
+  possibleParents[0] = document;
+  possibleParents[1] = this;
 
   for (var parentIndex = 0; parentIndex < possibleParents.length; parentIndex++) {
     var possibleParent = possibleParents[parentIndex];
-    if (typeof possibleParent != undefined) {
+    if (possibleParent != null) {
       defaultParent = possibleParent;
       break;
     }
   }
-  
+
   function createEventPolyfill(event, parent) {
-  
-    if (typeof parent == "undefined") {
+    if (parent == null) {
       parent = defaultParent;
     }
 
-    var parentName = parent.constructor.name;
-
-    if (typeof events[parentName] == "undefined") {
-      events[parentName] = new Object();
-    } else if (typeof events[parentName][event] != "undefined") {
-      return
+    if (parent.events == null) {
+      parent.events = new Object();
     }
-  
+
+    if (parent.events[event] != null) {
+      return;
+    }
+
     var eventPolyfill = new Object();
-  
     var functionQueue = new Array();
     eventPolyfill.functionQueue = functionQueue;
-  
+
     eventPolyfill.addEvent = function (functionReference) {
       var eventID = functionQueue.length;
       functionQueue[eventID] = functionReference;
       return eventID;
-    }
-  
+    };
+
     eventPolyfill.removeEvent = function (eventID) {
       var lastFunctionIndex = functionQueue.length - 1;
-  
-      for (var functionIndex = eventID; functionIndex < lastFunctionIndex; functionIndex++) {
+      var functionIndex = eventID;
+      while (functionIndex < lastFunctionIndex) {
         functionQueue[functionIndex] = functionQueue[functionIndex + 1];
+        functionIndex = functionIndex + 1;
       }
-  
       functionQueue.length = lastFunctionIndex;
-    }
-  
-    events[parentName][event] = eventPolyfill;
-  
-    parent[event] = function(context) {
-      for (var functionIndex = 0; functionIndex < functionQueue.length; functionIndex++) {
+    };
+
+    parent.events[event] = eventPolyfill;
+
+    parent[event] = function (context) {
+      var functionIndex = 0;
+      while (functionIndex < functionQueue.length) {
         functionQueue[functionIndex](context);
+        functionIndex = functionIndex + 1;
       }
     };
   }
-  
-  window.events = events;
-  window.createEventPolyfill = createEventPolyfill;
+
+  this.createEventPolyfill = createEventPolyfill;
 
   createEventPolyfill("onkeydown", document);
   createEventPolyfill("onresize", window);
 }();
-
