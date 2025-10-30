565ad420f71acc62cfa92ce2c10ac5c108078815
Reasae <reasae@proton.me>
2025-10-30 09:01:37 -0300

    Chapter 5: The One Who Hears Symphonies

---
+++ src/scripts/3.catcher.js
@@ -0,0 +1,41 @@
+function Catcher() {
+  var logger = document.createElement("input");
+  this.logger = logger;
+  logger.className = "catcher";
+  document.body.appendChild(logger);
+
+  this.listen();
+  createEventPolyfill("oninput", logger);
+
+  this.listenID;
+};
+
+Catcher.prototype.hear = function () {
+  this.logger.focus();
+}
+
+Catcher.prototype.listen = function () {
+  var self = this;
+
+  this.listenID = document.events.onkeydown.addEvent(function () {
+    self.hear();
+  }); 
+}
+
+Catcher.prototype.deafen = function () {
+  this.logger.blur();
+}
+
+Catcher.prototype.zone = function () {
+  if (typeof this.listenID != undefined) {
+    this.listenID = document.events.onkeydown.removeEvent(this.listenID);  
+  }
+}
+
+Catcher.prototype.stream = function (port) {
+  var self = this;
+
+  this.streamID = this.logger.events.oninput.addEvent(function () {
+    port(self.logger.value)
+  })
+}
